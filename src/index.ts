0101fe2c0d23f6dbbd8000495c2f7e519905203f
Hoang <hoangdh@omegatheme.com>
2025-02-16 15:14:40 +0700

    [Feat] install express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const name: any = 'king'
-console.log(name)
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
